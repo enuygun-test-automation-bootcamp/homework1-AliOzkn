82120e9dfd4a52681ceb2ae4f6ff9f1670c841ca
AliOzkn <alizkann@gmail.com>
2022-06-25 13:38:08 +0300

    commit

--- CuttingTools.java
+++ CuttingTools.java
@@ -1,7 +1,14 @@
 public abstract class CuttingTools {
-
-    public static double Vc,D,N,F,Fuc;
-    public static int Z;
+    /*
+    Vc:Kesme Hizi(m/min)
+    D: Kesici takim capi (mm)
+    S: Devir (rev/min)
+    F:Ilerleme (mm/min)
+    Fuc: 1 uc basina ilerleme (mm)
+    Z: Uc Sayisi
+    */
+    private static double Vc,D,S,F,Fuc;
+    private static int Z;
 
     public CuttingTools(double Vc, double D, double Fuc, int Z){
         this.Vc = Vc;
@@ -10,14 +17,19 @@ public abstract class CuttingTools {
         this.Z = Z;
     }
 
-    static double rpm(){
-
-        return N = ((Vc*1000)/(3.14*D));
+    private static double rpm(){
+        return S = ((Vc*1000)/(3.14*D)); //Devir Formulu
     }
-    static double feed(){
-
-        return  F = (Fuc*Z*N);
+    void showRpm(){
+        System.out.println("rpm: " + rpm());
     }
+    private static double feed(){
+        return  F = (Fuc*Z*S);    // Ilerleme Formulu
+    }
+    void showFeed(){
+        System.out.println("Feed: " + feed());
+    }
+
     abstract void drillInformation();
     abstract void endMillInformation();
 
--- Main.java
+++ Main.java
@@ -1,15 +1,16 @@
 public class Main {
 
     public static void main(String[] args) {
-        CuttingTools Drill = new Drill(400, 50,0.17,1);
-        System.out.println("rpm: " + CuttingTools.rpm());
-        System.out.println("Feed: " + CuttingTools.feed());
+        CuttingTools Drill = new Drill(375, 48,0.17,1);
+        Drill.showRpm();
+        Drill.showFeed();
         Drill.drillInformation();
         System.out.println("----------------------------------------------------------------------------");
 
-        CuttingTools EndMill = new EndMill(90,12,0.08,4);
-        System.out.println("rpm: " + CuttingTools.rpm());
-        System.out.println("Feed: " + CuttingTools.feed());
+        CuttingTools EndMill = new EndMill(90,12,0.08,3);
+        EndMill.showRpm();
+        EndMill.showFeed();
         EndMill.endMillInformation();
+
     }
 }
